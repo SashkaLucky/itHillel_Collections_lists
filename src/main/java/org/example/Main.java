c913f791de7e368ed650436c62a0e219be849530
Creepy.daffodil <Alexander26Peshkov@ukr.net>
2023-04-11 10:12:18 +0300

    PhoneBook

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,34 @@
-package org.example;public class Main {
+package org.example;
+
+import org.example.PhoneBook;
+
+import java.util.List;
+
+public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        PhoneBook phonebook = new PhoneBook();
+
+        phonebook.add(new PhoneBook.Record("John Smith", "0999966999"));
+        phonebook.add(new PhoneBook.Record("Jane Smith", "0669966999"));
+        phonebook.add(new PhoneBook.Record("John Doe", "0996699666"));
+
+        PhoneBook.Record record = phonebook.find("John Smith");
+        if (record != null) {
+            System.out.println("Found record: " + record.getName() + " - " + record.getPhoneNumber());
+        } else {
+            System.out.println("Record not found.");
+        }
+
+        List<PhoneBook.Record> records = phonebook.findAll("John Smith");
+        if (records != null) {
+            System.out.println("Found " + ((List<?>) records).size() + " records:");
+            for (PhoneBook.Record r : records) {
+                System.out.println(r.getName() + " - " + r.getPhoneNumber());
+            }
+        } else {
+            System.out.println("No records found.");
+        }
     }
+
 }
--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,2 +1,58 @@
-package org.example;public class PhoneBook {
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PhoneBook {
+
+    private List<Record> records;
+
+    public PhoneBook() {
+        records = new ArrayList<>();
+    }
+
+    public void add(Record record) {
+        records.add(record);
+    }
+
+    public Record find(String name) {
+        for (Record record : records) {
+            if (record.getName().equals(name)) {
+                return record;
+            }
+        }
+        return null;
+    }
+
+    public List<Record> findAll(String name) {
+        List<Record> result = new ArrayList<>();
+        for (Record record : records) {
+            if (record.getName().equals(name)) {
+                result.add(record);
+            }
+        }
+        return result.isEmpty() ? null : result;
+    }
+
+    public static class Record {
+        private String name;
+        private String phoneNumber;
+
+        public Record(String name, String phoneNumber) {
+            this.name = name;
+            this.phoneNumber = phoneNumber;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getPhoneNumber() {
+            return phoneNumber;
+        }
+
+        public void setPhoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+        }
+    }
 }
